553698499daf32d01e18a56bb22e709c6eb871f7
1s22s1 <1s22s1@example.com>
2023-04-14 09:03:10 +0900

    First commit

---
+++ lib/botanyuki.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'botanyuki/version'
+
+module Botanyuki
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/botanyuki_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Botanyuki do
+  it 'has a version number' do
+    expect(Botanyuki::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
